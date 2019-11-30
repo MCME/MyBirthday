c159e5cf790d5a4b98c5fc01c1764d1f6c0ba1c0
fraspace5 <simonagottardi@192.168.1.20>
2019-11-30 20:31:10 +0100

    Bungeecord

--- src/main/java/com/mcme/mybirthday/PluginData.java
+++ src/main/java/com/mcme/mybirthday/PluginData.java
@@ -20,12 +20,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.scheduler.BukkitRunnable;
 
 /**
  *
@@ -33,28 +42,156 @@ import lombok.Setter;
  */
 public class PluginData {
 
-    @Getter
-    @Setter
-    private static Map<UUID, BirthDayData> dataMap = new HashMap<>();
-
-    public static void saveData(File file) throws FileNotFoundException {
-        for (BirthDayData data : dataMap.values()) {
-
-            String storageData = data.serialize();
-            try (PrintWriter out = new PrintWriter(new FileOutputStream(file))) {
-                out.println(storageData);
+    public static synchronized ResultSet loadData(UUID uuid) throws SQLException {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                String statement = "SELECT uuid FROM BDATA WHERE uuid = " + uuid.toString();
+                ResultSet r = birthday.getPluginInstance().con.createStatement().executeQuery(statement);
+                // do database stuff here
+                new BukkitRunnable() {
+                    @Override
+                    public ResultSet run() {
+                        return r;
+                    }
+                }.runTaskAsynchronously(birthday.getPluginInstance());
             }
+        }.runTaskAsynchronously(birthday.getPluginInstance());
+
+    }
+
+    public static synchronized ResultSet loadAll() throws SQLException {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                String statement = "SELECT uuid FROM BDATA";
+                ResultSet r = birthday.getPluginInstance().con.createStatement().executeQuery(statement);
+                // do database stuff here
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        return r;
+                    }
+                }.runTaskAsynchronously(birthday.getPluginInstance());
+            }
+        }.runTaskAsynchronously(birthday.getPluginInstance());
+
+    }
+
+    public static synchronized void updateData(final UUID uuid, final Calendar date, final Boolean bol, final Long cooldown) throws SQLException {
+
+        if (birthday.getPluginInstance().isConnect()) {
+
+            new BukkitRunnable() {
+
+                @Override
+                public void run() {
+
+                    String statement = "SELECT uuid FROM BDATA WHERE uuid = " + uuid.toString();
+                    ResultSet r = null;
+                    try {
+                        r = birthday.getPluginInstance().con.createStatement().executeQuery(statement);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(PluginData.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    try {
+                        if (r.first()) {
+                            Date d = new Date(date.getTimeInMillis());
+                            String stat = "UPDATE INTO BDATA (uuid, date, particles, cooldown) VALUES  (" + uuid + ", " + d + ", " + bol + ", " + cooldown + ") WHERE uuid = " + uuid;
+                            birthday.getPluginInstance().con.createStatement().execute(stat);
+
+                        } else {
+                            Date d = new Date(date.getTimeInMillis());
+                            String stat = "INSERT INTO BDATA (uuid, date, particles, cooldown) VALUES  (" + uuid + ", " + d + ", " + bol + ", " + cooldown + ")";
+                            birthday.getPluginInstance().con.createStatement().execute(stat);
+                        }
+                    } catch (SQLException ex) {
+                        Logger.getLogger(PluginData.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+            }.runTaskAsynchronously(birthday.getPluginInstance());
+
+        } else {
+            birthday.getPluginInstance().openConnection();
         }
     }
 
-    public static void loadData(File file) throws FileNotFoundException {
-        try (Scanner scanner = new Scanner(file)) {
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
-                BirthDayData data = new BirthDayData(line);
-                dataMap.put(data.getUuid(), data);
-            }
+    public static synchronized void insertData(final UUID uuid, final Calendar date, final Boolean bol, final Long cooldown) throws SQLException {
+        if (birthday.getPluginInstance().isConnect()) {
+            new BukkitRunnable() {
+
+                @Override
+                public void run() {
+
+                    Date d = new Date(date.getTimeInMillis());
+                    String stat = "INSERT INTO BDATA (uuid, date, particles, cooldown) VALUES  (" + uuid + ", " + d + ", " + bol + ", " + cooldown + ")";
+                    try {
+                        birthday.getPluginInstance().con.createStatement().executeQuery(stat);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(PluginData.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+            }.runTaskAsynchronously(birthday.getPluginInstance());
+
+        } else {
+            birthday.getPluginInstance().openConnection();
         }
+    }
+
+    public static synchronized void deleteData(final UUID uuid) throws SQLException {
+        if (birthday.getPluginInstance().isConnect()) {
+            new BukkitRunnable() {
+
+                @Override
+                public void run() {
+
+                    String stat = "DELETE BDATA WHERE " + uuid;
+                    try {
+                        birthday.getPluginInstance().con.createStatement().execute(stat);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(PluginData.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+            }.runTaskAsynchronously(birthday.getPluginInstance());
+
+        } else {
+            birthday.getPluginInstance().openConnection();
+        }
+    }
+
+    public static synchronized boolean exists(UUID uuid) throws SQLException {
+        if (birthday.getPluginInstance().isConnect()) {
+            String statement = "SELECT uuid FROM BDATA WHERE uuid = " + uuid.toString();
+            ResultSet r = birthday.getPluginInstance().con.createStatement().executeQuery(statement);
+
+            if (r.first()) {
+
+                return true;
+
+            } else {
+
+                return false;
+
+            }
+
+        } else {
+            birthday.getPluginInstance().openConnection();
+            return false;
+        }
+    }
+
+    public static Calendar convert(Date date) {
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.setTime(date);
+
+        return cal;
+
     }
 
 }
