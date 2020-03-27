437584db1aaf4c6bd95824d6996a8c7adce21ed2
EriolEandur <eriol_eandur@gmx.de>
2020-03-27 15:58:44 +0100

    manual merge into master

---
+++ src/main/java/com/mcme/mybirthday/MyBirthday.java
@@ -0,0 +1,327 @@
+/*
+ *Copyright (C) 2020 MCME (Fraspace5)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.mcme.mybirthday;
+
+import org.bukkit.plugin.Plugin;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.TimeZone;
+import java.util.logging.Level;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Fraspace5
+ */
+public class MyBirthday extends JavaPlugin implements Listener {
+
+    Logger Logger = Bukkit.getLogger();
+    ConsoleCommandSender clogger = this.getServer().getConsoleSender();
+
+    @Getter
+    public Connection con;
+
+    @Getter
+    String discordChannel = this.getConfig().getString("dchannel");
+    @Setter
+    @Getter
+    boolean discordFound;
+
+    @Getter
+    public static MyBirthday pluginInstance;
+
+    private void checkUpdate() {
+        final UpdaterCheck updater = new UpdaterCheck(this);
+    }
+
+    @Override
+    public void onEnable() {
+        pluginInstance = this;
+        this.saveDefaultConfig();
+        this.getConfig().options().copyDefaults();
+        clogger.sendMessage(ChatColor.GREEN + "---------------------------------------");
+        clogger.sendMessage(ChatColor.YELLOW + "MyBirthday Plugin v" + this.getDescription().getVersion() + " Enabled");
+        clogger.sendMessage(ChatColor.GREEN + "---------------------------------------");
+
+        getCommand("birthday").setExecutor(new command());
+        getCommand("birthday").setTabCompleter(new command());
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+        if (this.getConfig().getBoolean("findupdates")) {
+
+            checkUpdate();
+        }
+
+        try {
+            openConnection();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            clogger.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.YELLOW + "MyBirthday" + ChatColor.DARK_GRAY + "] - " + ChatColor.RED + "Database error! (MyBirthday)");
+            Logger.getLogger(MyBirthday.class.getName()).log(Level.SEVERE, null, ex);
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+        ConnectionRunnable();
+        SetListRunnable();
+        ShowListRunnable();
+        CheckDiscord();
+
+    }
+
+    public void openConnection() throws SQLException {
+        if (con != null && !con.isClosed()) {
+            return;
+        }
+        if (MyBirthday.getPluginInstance().password.equalsIgnoreCase("default")) {
+            clogger.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.YELLOW + "MyBirthday" + ChatColor.DARK_GRAY + "] - " + ChatColor.YELLOW + "Plugin INITIALIZED, change database information!");
+            Bukkit.getPluginManager().disablePlugin(this);
+        } else {
+
+            con = DriverManager.getConnection("jdbc:mysql://" + MyBirthday.getPluginInstance().host + ":"
+                    + MyBirthday.getPluginInstance().port + "/"
+                    + MyBirthday.getPluginInstance().database + "?useSSL=false&allowPublicKeyRetrieval=true",
+                    MyBirthday.getPluginInstance().username,
+                    MyBirthday.getPluginInstance().password);
+            clogger.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.YELLOW + "MyBirthday" + ChatColor.DARK_GRAY + "] - " + ChatColor.GREEN + "Database Found! ");
+
+            new BukkitRunnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        String statement = "CREATE TABLE IF NOT EXISTS b_data (uuid VARCHAR(50), particles BOOLEAN, cooldown LONG, year INT, month INT, day INT) ;";
+                        String statement2 = "CREATE TABLE IF NOT EXISTS player_data (uuid VARCHAR(50), bool BOOLEAN) ;";
+                        con.createStatement().execute(statement);
+                        con.createStatement().execute(statement2);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(MyBirthday.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }.runTaskAsynchronously(MyBirthday.getPluginInstance());
+        }
+
+    }
+
+    public void CheckDiscord() {
+
+        Plugin discordPlugin = Bukkit.getServer().getPluginManager().getPlugin("DiscordSRV");
+        if (discordPlugin == null) {
+            this.setDiscordFound(Boolean.FALSE);
+            clogger.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.YELLOW + "MyBirthday" + ChatColor.DARK_GRAY + "] - " + ChatColor.RED + "DiscordSRV not found!");
+
+        } else if (discorden = true) {
+            this.setDiscordFound(Boolean.TRUE);
+            DiscordRunnable();
+            clogger.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.YELLOW + "MyBirthday" + ChatColor.DARK_GRAY + "] - " + ChatColor.GREEN + "DiscordSRV found!");
+        }
+
+    }
+
+    public void ConnectionRunnable() {
+
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                try {
+                    if (!con.isValid(2)) {
+
+                        openConnection();
+
+                    }
+                } catch (SQLException ex) {
+                    Logger.getLogger(MyBirthday.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+
+        }.runTaskTimer(MyBirthday.getPluginInstance(), 60L, 100L);
+
+    }
+
+    public void DiscordRunnable() {
+
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+
+                TimeZone tim = TimeZone.getTimeZone("Europe/London");
+                Calendar cal = Calendar.getInstance(tim);
+
+                if (cal.get(Calendar.HOUR_OF_DAY) == MyBirthday.getPluginInstance().getConfig().getInt("time.hours")
+                        && cal.get(Calendar.MINUTE) == MyBirthday.getPluginInstance().getConfig().getInt("time.minutes")) {
+
+                    PluginData.createMessageandSend();
+
+                }
+
+            }
+
+        }.runTaskTimer(MyBirthday.getPluginInstance(), 0L, 1200L);
+
+    }
+
+    @Override
+    public void onDisable() {
+
+        clogger.sendMessage(ChatColor.RED + "---------------------------------------");
+        clogger.sendMessage(ChatColor.YELLOW + "MyBirthday Plugin v" + this.getDescription().getVersion() + " Disabled");
+        clogger.sendMessage(ChatColor.RED + "---------------------------------------");
+
+    }
+    @Getter
+    @Setter
+    public boolean connect;
+    @Getter
+    List<UUID> todaybirthday = new ArrayList<>();
+    @Getter
+    HashMap<UUID, Long> coolsure = new HashMap<>();
+    @Getter
+    Calendar call = Calendar.getInstance();
+    @Getter
+    boolean liston = this.getConfig().getBoolean("broadcastlist");
+    @Getter
+    int broadcastlistevery = this.getConfig().getInt("broadcastlistevery");
+    @Getter
+    boolean particles = this.getConfig().getBoolean("particles");
+    @Getter
+    String message = this.getConfig().getString("privacymessage");
+    @Getter
+    String host = this.getConfig().getString("host");
+    @Getter
+    String port = this.getConfig().getString("port");
+    @Getter
+    String database = this.getConfig().getString("database");
+    @Getter
+    Boolean discorden = this.getConfig().getBoolean("dmessage");
+    @Getter
+    String username = this.getConfig().getString("username");
+    @Getter
+    String password = this.getConfig().getString("password");
+    @Getter
+    boolean mactive = this.getConfig().getBoolean("messageactive");
+    @Getter
+    String coold = this.getConfig().getString("wait-time");
+    @Getter
+    long cooldown1 = this.getConfig().getLong("wait-time");
+    @Getter
+    boolean playeragebool = this.getConfig().getBoolean("showplayerage");
+
+    public void SetListRunnable() {
+
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+
+                try {
+                    SetTodayBirthdays();
+                } catch (SQLException ex) {
+                    Logger.getLogger(MyBirthday.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+        }.runTaskTimer(this, 0L, 600L);
+
+    }
+
+    public void ShowListRunnable() {
+
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                PluginData.showlist();
+
+            }
+
+        }.runTaskTimer(this, broadcastlistevery * 60 * 20 * 1000000, broadcastlistevery * 60 * 20);
+
+    }
+
+    public void OtherPeopleBirthday(UUID nameplayer, PlayerJoinEvent e) throws SQLException {
+
+        PluginData.otherpeopleSQL(nameplayer, e);
+
+    }
+
+    public void SetTodayBirthdays() throws SQLException {
+
+        PluginData.today();
+
+    }
+
+    public void ShowList(Player pl, PlayerJoinEvent e) throws SQLException {
+
+        PluginData.showListSQL(pl, e);
+
+    }
+
+    @EventHandler
+    public void onJoin(final PlayerJoinEvent e) throws FileNotFoundException, SQLException {
+
+        final Player pl = e.getPlayer();
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    String statement = "SELECT * FROM " + MyBirthday.getPluginInstance().database + ".player_data WHERE uuid = '" + pl.getUniqueId().toString() + "' ;";
+
+                    final ResultSet r = MyBirthday.getPluginInstance().con.createStatement().executeQuery(statement);
+                    if (r.first()) {
+                        if (r.getBoolean("bool")) {
+                            PluginData.onJoinSQL(e, true);
+                        } else {
+                            PluginData.onJoinSQL(e, false);
+                        }
+                    } else {
+                        String s = "INSERT INTO " + MyBirthday.getPluginInstance().database + ".player_data (uuid, bool) VALUES ('" + pl.getUniqueId().toString() + "', 0 ); ";
+                        MyBirthday.getPluginInstance().con.createStatement().executeUpdate(s);
+                        PluginData.onJoinSQL(e, false);
+                    }
+                } catch (SQLException ex) {
+                    Logger.getLogger(command.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
+        }.runTaskAsynchronously(MyBirthday.getPluginInstance());
+
+    }
+}
---
+++ src/main/java/com/mcme/mybirthday/command.java
@@ -0,0 +1,272 @@
+/*
+ *Copyright (C) 2020 MCME (Fraspace5)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.mcme.mybirthday;
+
+import java.sql.ResultSet;
+import java.util.Calendar;
+import java.util.UUID;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.command.TabExecutor;
+
+/**
+ *
+ * @author Fraspace5
+ */
+public class command implements CommandExecutor, TabExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (sender instanceof Player) {
+
+            final Player pl = (Player) sender;
+            final UUID uuid = pl.getUniqueId();
+            Calendar call = Calendar.getInstance();
+
+            if (args.length < 1) {
+
+                pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.RED + " Invalid Usage ! Type /birthday help for information");
+
+            } else if (args[0].equalsIgnoreCase("set")) {
+                if (pl.hasPermission("mybirthday.*") || pl.hasPermission("mybirthday.set")) {
+                    if (args.length == 4) {
+
+                        try {
+                            PluginData.setSQL(uuid, args, pl);
+                        } catch (SQLException ex) {
+                            Logger.getLogger(command.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+
+                    } else {
+
+                        pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.RED + " Not enough argouments! Type /birthday help");
+                    }
+
+                } else {
+                    pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.RED + " You don't have enough permissions to use this command");
+
+                }
+
+            } else if (args[0].equalsIgnoreCase("help")) {
+
+                pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.YELLOW + " To set your birthday write /birthday set dd mm yyyy");
+
+            } else if (args[0].equalsIgnoreCase("change")) {
+                if (pl.hasPermission("mybirthday.change")) {
+                    if (args.length > 2) {
+                        OfflinePlayer p = Bukkit.getOfflinePlayer(args[1]);
+
+                        try {
+                            PluginData.setSQLStaff(p.getUniqueId(), args, pl);
+                        } catch (SQLException ex) {
+                            Logger.getLogger(command.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                } else {
+                    pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.RED + " You don't have enough permissions to use this command");
+                }
+
+            } else if (args[0].equalsIgnoreCase("particles")) {
+                if (pl.hasPermission("mybirthday.*") || pl.hasPermission("mybirthday.particles")) {
+                    if (args.length == 2) {
+                        PluginData.particlesSQL(uuid, args, pl);
+                    } else {
+                        pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.RED + " Not enough argouments! Type /birthday help");
+                    }
+                } else {
+                    pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.RED + " You don't have enough permissions to use this command");
+                }
+
+            } else if (args[0].equalsIgnoreCase("removedatab")) {
+                if (pl.hasPermission("mybirthday.*") || pl.hasPermission("mybirthday.delete")) {
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                String statement = "SELECT * FROM " + MyBirthday.getPluginInstance().database + ".b_data WHERE uuid = '" + uuid.toString() + "' ;";
+
+                                final ResultSet r = MyBirthday.getPluginInstance().con.createStatement().executeQuery(statement);
+                                if (r.first() && r.getInt("year") != 1970) {
+                                    if (MyBirthday.getPluginInstance().getCoolsure().containsKey(uuid) && MyBirthday.getPluginInstance().getCoolsure().get(uuid) > System.currentTimeMillis()) {
+                                        pl.sendMessage((ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.YELLOW + " All your data has been removed from our databases"));
+                                        Calendar cal = Calendar.getInstance();
+                                        cal.set(1970, 1, 1);
+                                        try {
+                                            Long l = (System.currentTimeMillis() + (MyBirthday.getPluginInstance().getCooldown1() * 3600) * 1000);
+                                            PluginData.updateData(uuid, cal, Boolean.FALSE, l);
+                                            MyBirthday.getPluginInstance().getCoolsure().remove(uuid);
+                                        } catch (SQLException ex) {
+                                            Logger.getLogger(command.class.getName()).log(Level.SEVERE, null, ex);
+                                        }
+
+                                    } else {
+                                        long time = System.currentTimeMillis() + (30 * 1000);
+                                        MyBirthday.getPluginInstance().getCoolsure().put(uuid, time);
+                                        pl.sendMessage((ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.YELLOW + " Are you sure? Retype /birthday removedatab to confirm"));
+                                    }
+
+                                } else {
+                                    pl.sendMessage((ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.YELLOW + " Nothing to remove"));
+                                }
+                            } catch (SQLException ex) {
+                                Logger.getLogger(command.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+
+                        }
+                    }.runTaskAsynchronously(MyBirthday.getPluginInstance());
+
+                } else {
+                    pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.RED + " You don't have enough permissions to use this command");
+                }
+            } else if (args[0].equalsIgnoreCase("ignore")) {
+
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            String statement = "SELECT * FROM " + MyBirthday.getPluginInstance().database + ".player_data WHERE uuid = '" + uuid.toString() + "' ;";
+
+                            final ResultSet r = MyBirthday.getPluginInstance().con.createStatement().executeQuery(statement);
+                            if (r.first()) {
+                                if (r.getBoolean("bool")) {
+                                    String stat = "UPDATE " + MyBirthday.getPluginInstance().database + ".player_data SET bool = 0 ;";
+                                    pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.GREEN + "Now you aren't ignoring my plugin, thanks");
+                                    MyBirthday.getPluginInstance().con.createStatement().executeUpdate(stat);
+                                } else {
+                                    String stat = "UPDATE " + MyBirthday.getPluginInstance().database + ".player_data SET bool = 1 ;";
+                                    pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.GREEN + "You are ignoring MyBirthday plugin");
+                                    MyBirthday.getPluginInstance().con.createStatement().executeUpdate(stat);
+                                }
+                            } else {
+                                String s = "INSERT INTO " + MyBirthday.getPluginInstance().database + ".player_data (uuid, bool) VALUES ('" + uuid.toString() + "', 1); ";
+                                pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.GREEN + "You are ignoring MyBirthday plugin");
+                                MyBirthday.getPluginInstance().con.createStatement().executeUpdate(s);
+                            }
+                        } catch (SQLException ex) {
+                            Logger.getLogger(command.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+
+                    }
+                }.runTaskAsynchronously(MyBirthday.getPluginInstance());
+
+            } else {
+
+                pl.sendMessage(ChatColor.GOLD.BOLD + "[MyBirthday] :" + ChatColor.RED + " Invalid Usage ! Type /birthday help for information");
+
+            }
+
+        } else {
+            System.out.println("You can't use this command in console");
+
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        Player pl = (Player) sender;
+        List<String> arguments = new ArrayList<>();
+        arguments.add("set");
+        arguments.add("help");
+        arguments.add("particles");
+        arguments.add("ignore");
+        arguments.add("removedatab");
+        if (pl.hasPermission("mybirthday.*") || pl.hasPermission("mybirthday.staff")) {
+            arguments.add("change");
+        }
+        List<String> Flist = new ArrayList<>();
+        List<String> particles = new ArrayList<>();
+        List<String> day = new ArrayList<>();
+        List<String> month = new ArrayList<>();
+        List<String> year = new ArrayList<>();
+        if (args.length == 1) {
+            for (String s : arguments) {
+                if (s.toLowerCase().startsWith(args[0].toLowerCase())) {
+                    Flist.add(s);
+                }
+            }
+            return Flist;
+        } else if (args.length == 2) {
+            if (args[0].equalsIgnoreCase("particles")) {
+
+                List<String> a = Arrays.asList("on", "off");
+                for (String s : a) {
+                    if (s.toLowerCase().startsWith(args[1].toLowerCase())) {
+                        particles.add(s);
+                    }
+                }
+                return particles;
+
+            } else if (args[0].equalsIgnoreCase("set")) {
+
+                List<String> a = Arrays.asList("day");
+                for (String s : a) {
+                    if (s.toLowerCase().startsWith(args[1].toLowerCase())) {
+                        day.add(s);
+                    }
+                }
+                return day;
+
+            } else {
+                return null;
+            }
+
+        } else if (args.length == 3) {
+            if (args[0].equalsIgnoreCase("set")) {
+                List<String> a = Arrays.asList("month");
+                for (String s : a) {
+                    if (s.toLowerCase().startsWith(args[2].toLowerCase())) {
+                        month.add(s);
+                    }
+                }
+                return month;
+            } else {
+                return null;
+            }
+        } else if (args.length == 4) {
+            if (args[0].equalsIgnoreCase("set")) {
+                List<String> a = Arrays.asList("year");
+                for (String s : a) {
+                    if (s.toLowerCase().startsWith(args[3].toLowerCase())) {
+                        year.add(s);
+                    }
+                }
+                return year;
+            } else {
+                return null;
+            }
+        } else {
+
+            return null;
+        }
+    }
+
+}
